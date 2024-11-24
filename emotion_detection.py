af68212e51125b78455846178386589acbab048b
czhinojosa <chinojosa@edutecno.com>
2024-11-24 02:36:01 -0500

    task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -6,7 +7,24 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
 
+    formatted_response = json.loads(response.text)
 
-    return response.text
+    emotion_scores = {}
+    if response.status_code == 400:
+        emotion_scores =  {
+            'anger':None,
+            'disgust':None,
+            'fear':None,
+            'joy':None,
+            'sadness':None,
+            'dominant_emotion':None}
+        return emotion_scores
 
-print(emotion_detector("I love this new technology.")) 
+
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    emotion_scores['dominant_emotion'] = dominant_emotion
+
+    return emotion_scores
+
+print(emotion_detector("I am so happy I am doing this"))
